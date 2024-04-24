035eed463eec3a92a522a36e8ab81d61cabc5311
HumbleG0d <humbleg0d7642@gmail.com>
2024-04-23 22:23:39 -0500

    aAdd class Coins

--- Conversor_Monedas/src/main/java/org/view/Core/Coins.java
+++ Conversor_Monedas/src/main/java/org/view/Core/Coins.java
@@ -1,2 +1,22 @@
-package org.view.Core;public class Coins {
+package org.view.Core;
+
+public class Coins {
+  private String code; // USD, EUR, GBP, etc
+  private double value; // 1 USD = 1.0, 1 EUR = 1.2, 1 GBP = 1.4, etc
+
+  // Constructor
+  public Coins(String code, double value) {
+    this.code = code;
+    this.value = value;
+  }
+
+  // Getters
+  public String getCode() {
+    return code;
+  }
+
+  // Getters
+ public double getValue() {
+    return value;
+  }
 }
