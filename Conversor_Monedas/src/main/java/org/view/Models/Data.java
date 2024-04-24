5014e7be5af3c0f66f88cde3d4d0a8f0bdd69e0b
HumbleG0d <humbleg0d7642@gmail.com>
2024-04-23 22:30:20 -0500

    Add logic to return coin data

--- Conversor_Monedas/src/main/java/org/view/Models/Data.java
+++ Conversor_Monedas/src/main/java/org/view/Models/Data.java
@@ -1,7 +1,7 @@
 package org.view.Models;
 
 import com.google.gson.*;
-import org.view.Logical.AllCoins;
+import org.view.Logic.AllCoins;
 import org.view.Services.ConsumirAPI;
 
 import java.io.FileNotFoundException;
@@ -10,9 +10,17 @@ import java.io.IOException;
 import java.io.Reader;
 import java.util.Map;
 
+public class Data {
 
-public class BDD {
-  public Map<String, Object> getCoins(ConsumirAPI api) throws IOException, InterruptedException {
+  private final ConsumirAPI api; //Variable que almacena la instancia de la clase ConsumirAPI
+
+  //Constructor de la clase
+  public Data() {
+    this.api = new ConsumirAPI(System.getenv("API_KEY"));
+  }
+
+  //Método que obtiene las monedas
+  public Map<String, Object> getCoins() throws IOException, InterruptedException {
 
     Gson gson = new GsonBuilder()
             .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
@@ -20,11 +28,11 @@ public class BDD {
     Gson gson2 = new Gson();
 
     AllCoins allCoins = gson.fromJson(api.getRequest() ,AllCoins.class);
-    //JsonObject jsonObject = gson2.fromJson(allCoins.conversion_rates().toString(), JsonObject.class);
     Map<String , Object>  map = gson2.fromJson(allCoins.conversion_rates().toString(), Map.class);
     return map;
   }
 
+  //Método que obtiene el código de la moneda
   public Map<String , Object> getCurrencyCode() throws FileNotFoundException {
     String file = "C:\\Users\\sergi\\OneDrive\\Escritorio\\Challenge_Conversor_de_Monedas\\Conversor_Monedas\\src\\main\\java\\org\\view\\JSON\\Curreny_Code.json";
     Gson gson = new Gson();
