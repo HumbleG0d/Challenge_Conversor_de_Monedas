3ffef1fec82b53d7cc3eed6c6046dd2f43f2415e
HumbleG0d <95192731+HumbleG0d@users.noreply.github.com>
2024-04-27 13:54:37 -0500

    Update ValidarCantidad.java

--- Conversor_Monedas/src/main/java/org/view/Validator/ValidarCantidad.java
+++ Conversor_Monedas/src/main/java/org/view/Validator/ValidarCantidad.java
@@ -1,2 +1,6 @@
-package org.view.Validator;public class ValidarCantidad {
+package org.view.Validator;
+public class ValidarCantidad {
+  public static boolean validValue(double cantidad) {
+    return cantidad > 0;
+  }
 }
